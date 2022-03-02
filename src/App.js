65ee6126c9d66ce3b8ee8131700e2218cbeef756
kamiada <kamiada528@gmail.com>
2022-03-02 20:53:57 +0000

    make page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,3 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import { Menu } from "./pages";
-
-// export default class App extends React.Component{
-//     render(){
-//         return(
-//             <div><Menu /> </div>
-//         )
-//     }
-// }
-// ReactDOM.render(<App />, document.getElementById('app'))
-
 import React from 'react';
 import './index.css';
 import { Menu } from "./pages";
