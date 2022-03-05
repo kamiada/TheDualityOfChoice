b4b8da882cb5e4843409c6501455efa6bf8c88f8
kamiada <kamiada528@gmail.com>
2022-03-05 10:38:25 +0000

    minor changes

--- src/pages/Menu/Menu.jsx
+++ src/pages/Menu/Menu.jsx
@@ -1,16 +1,12 @@
 import React from "react";
-import { Scene_1, Recipe, Game, Final, BadEnding, GoodEnding } from "..";
 import {
-  BrowserRouter as Router,
-  Link,
-  Routes,
+  Link
 } from "react-router-dom";
 import "../pages.scss";
 
 function Menu() {
   return (
-    <div>
-    Here
+    <div className="container_menu">
       <h1>The Duality of Choice</h1>
       <nav>
         <Link to="/scene">Start Game</Link>
